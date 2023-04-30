160cc8ce046309af8c61250e205e155ce31d0636
ABDNOAMAN <114725229+ABDNOAMAN@users.noreply.github.com>
2023-04-30 07:08:17 +0200

    Update settings.py

--- bilbasen/settings.py
+++ bilbasen/settings.py
@@ -9,7 +9,20 @@ DOWNLOAD_HANDLERS = {
     "https": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
 }
 
+# Add Your ScrapeOps API Key
+SCRAPEOPS_API_KEY = 'e4ea08af-ef35-4354-885d-e75b34979a52'
 
+# Add In The ScrapeOps Extension
+EXTENSIONS = {
+        'scrapeops_scrapy.extension.ScrapeOpsMonitor': 500, 
+        }
+ITEM_PIPELINES = {'stack.pipelines.MongoDBPipeline':300, }
+
+# Update The Download Middlewares
+DOWNLOADER_MIDDLEWARES = {
+'scrapeops_scrapy.middleware.retry.RetryMiddleware': 550,
+'scrapy.downloadermiddlewares.retry.RetryMiddleware': None,
+}
 OBOTSTXT_OBEY = True
 
 REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
@@ -19,6 +32,6 @@ TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 FEED_EXPORT_ENCODING = "utf-8"
 
 PLAYWRIGHT_LAUNCH_OPTIONS = {
-    "headless": False,
+    "headless": True,
     #"timeout": 2000 * 1000,  # 20 seconds
 }
